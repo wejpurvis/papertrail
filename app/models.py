8f96cc3bdc532cdc89114df9cabca3b301b8bbc4
Will Purvis <wej.purvis@gmail.com>
2026-02-19 14:04:33 +0100

    Define Paper tortoise model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
-# Tortoise ORM database modelsß
+# Tortoise ORM database models
+from tortoise.models import Model
+from tortoise import fields
+
+
+class Paper(Model):
+    id = fields.IntField(primary_key=True)
+    title = fields.CharField(max_length=500)
+    abstract = fields.TextField()
+    authors = fields.JSONField()  # list of strings
+    year = fields.IntField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        table = "papers"
