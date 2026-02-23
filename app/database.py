6b06d791ec13bede7d0217d01a5c2bb9b199b6ca
Will Purvis <wej.purvis@gmail.com>
2026-02-23 11:59:50 +0100

    added module description

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
-# Tortoise configuration to use SQLite
-
+"""
+Defines the Tortoise ORM configuration for the app. This is imported and used in `main.py` to initialize the database connection and generate schemas on startup.
+"""
 
 TORTOISE_ORM = {
     "connections": {"default": "sqlite://./papertrail.db"},
